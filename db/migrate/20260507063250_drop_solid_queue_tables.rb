93c7ad537d342505d2e9896f58b7c6f3b33afc9e
DaminiKarvande-ParamVista~ <damini.karvande@paramvista.com>
2026-05-07 12:26:08 +0530

    remove: completely remove solid_queue from application

--- db/migrate/20260507063250_drop_solid_queue_tables.rb
+++ db/migrate/20260507063250_drop_solid_queue_tables.rb
@@ -1,4 +1,17 @@
-class DropSolidQueueTables < ActiveRecord::Migration[8.1]
-  def change
+class DropSolidQueueTables < ActiveRecord::Migration[8.0]
+  def up
+    # Drop Solid Queue tables in correct dependency order
+    drop_table :solid_queue_blocked_jobs, if_exists: true
+    drop_table :solid_queue_failed_jobs, if_exists: true
+    drop_table :solid_queue_ready_executions, if_exists: true
+    drop_table :solid_queue_scheduled_jobs, if_exists: true
+    drop_table :solid_queue_pauses, if_exists: true
+    drop_table :solid_queue_jobs, if_exists: true
+    drop_table :solid_queue_processes, if_exists: true
+    drop_table :solid_queue_executions, if_exists: true
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
